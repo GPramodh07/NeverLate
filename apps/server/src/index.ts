d3b3dce36a51ee14d56c5c9fe26aa6700db99127
GPramodh07 <gotivadapramodh@gmail.com>
2026-05-26 14:23:07 +0530

    server error in react

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.send("Hello, world!"));
+app.get("/", (req, res) => {
+  res.send("Hello from @neverlate/server!");
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
